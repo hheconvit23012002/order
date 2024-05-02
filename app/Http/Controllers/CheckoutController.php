b1ae310246514de5ae4d4390570912a20d22465f
Huấn Nguyễn Danh <huannd@kaopiz.com>
2024-05-02 14:37:43 +0700

    fix-desgin

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -8,6 +8,7 @@ use App\Models\Order;
 use App\Models\OrderDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class CheckoutController extends Controller
 {
@@ -16,22 +17,63 @@ class CheckoutController extends Controller
             DB::beginTransaction();
             $user = $request->get('user');
             $item = $request->get('item');
-            $order = new Order([
-                'user_id' => $user["id"],
-            ] + $request->except('user','item'));
-            $order->save();
-            $orderDetail = [];
-            foreach ($item as $each){
-                $orderDetail[] = [
-                    'order_id' => $order->id,
-                    'product_id' => $each["product_id"],
-                    'number' => $each["number"],
-                    'price' => $each["price"],
-                    'name_product' => $each["name_product"],
-                    'image_product' => $each["image_product"],
-                ];
+
+            $response = Http::withHeaders([
+                "Accept" => "application/json",
+            ])->post('http://127.0.0.1:5053/api/book/checkValidate',[
+                'item' => $item
+            ]);
+
+            if($response->status() !== 200){
+                throw new \Exception('validate product failed');
             }
-            OrderDetail::insert($orderDetail);
+
+
+            $response = Http::withHeaders([
+                "Accept" => "application/json",
+            ])->get('http://127.0.0.1:5052/api/checkCard');
+
+            if($response->status() !== 200){
+                throw new \Exception('Card not found');
+            }
+
+
+            $response = Http::withHeaders([
+                "Accept" => "application/json",
+            ])->post('http://127.0.0.1:5053/api/book/updateProduct',[
+                'item' => $item
+            ]);
+
+            if($response->status() !== 200){
+                throw new \Exception('update product failed');
+            }
+
+            $order = [
+                    'user_id' => $user["id"],
+                ] + $request->except('user','item');
+            $response = Http::withHeaders([
+                "Accept" => "application/json",
+            ])->post('http://127.0.0.1:5051/api/saveOrder',[
+                'order' => $order,
+                'item' => $item
+            ]);
+
+            if($response->status() !== 200){
+                throw new \Exception('Save order failed');
+            }
+            $orderId = $response->json();
+
+
+            $response = Http::withHeaders([
+                "Accept" => "application/json",
+            ])->post('http://127.0.0.1:5052/api/payment',[
+                'orderId' => $orderId['order_id'],
+            ]);
+
+            if($response->status() !== 200){
+                throw new \Exception('Payment failed');
+            }
+
             PingJob::dispatch($user);
             DB::commit();
             return response()->json([
@@ -39,7 +81,6 @@ class CheckoutController extends Controller
             ], 200);
         }catch (\Exception $e){
             DB::rollBack();
-            dd($e);
             return response()->json([
                 "message" => $e->getMessage(),
             ],500);
