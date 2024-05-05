124c899517d8318a6c0cd0649ec197b14d08909e
Huấn Nguyễn Danh <huannd@kaopiz.com>
2024-05-05 14:15:59 +0700

    config-queue

--- config/queue.php
+++ config/queue.php
@@ -83,7 +83,7 @@ return [
                     'port' => env('RABBITMQ_PORT', 5672),
                     'user' => env('RABBITMQ_USER', 'guest'),
                     'password' => env('RABBITMQ_PASSWORD', 'guest'),
-                    'vhost' => env('RABBITMQ_VHOST', 'laravel_email'),
+                    'vhost' => env('RABBITMQ_VHOST', 'laravel_consume'),
                 ],
             ],
 
